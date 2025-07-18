08afcbee75f31d1fe3377fc274962024362fbc94
Kr0issant <93475036+Kr0issant@users.noreply.github.com>
2025-07-18 16:17:43 +0530

    erase randomize and paint feature

--- script.js
+++ script.js
@@ -3,6 +3,21 @@ const canvas_size_label = document.querySelector("output");
 
 const gridbox = document.querySelector(".gridbox");
 
+const clear_btn = document.querySelector(".clear-btn");
+const erase_toggle = document.querySelector("#erase-toggle");
+const randomize_toggle = document.querySelector("#randomize-toggle");
+const color_selector = document.querySelector("#color-selector");
+
+var pointerDown = false;
+
+document.body.addEventListener('pointerdown', () => {
+  pointerDown = true;
+});
+
+document.body.addEventListener('pointerup', () => {
+  pointerDown = false;
+});
+
 canvas_size.addEventListener("input", () => {
     canvas_size_label.value = canvas_size.value;
     redraw_grid(canvas_size.value);
@@ -16,5 +31,36 @@ function redraw_grid() {
     gridbox.innerHTML = row_unit.repeat(size);
 }
 
-canvas_size.value = 16;
+gridbox.addEventListener("pointerover", (e) => {
+    if (e.target.classList.contains('grid-unit')) {
+        if (pointerDown) {
+            if (erase_toggle.checked) {
+                e.target.style.backgroundColor = "#ffffff"
+            } else if (randomize_toggle.checked) {
+                const randomColor = Math.floor(Math.random() * 16777215).toString(16); 
+                e.target.style.backgroundColor = `#${randomColor.padStart(6, '0')}`;
+            } else {
+                e.target.style.backgroundColor = color_selector.value;
+            }
+        }
+    }
+}, true);
+gridbox.addEventListener("pointerdown", (e) => {
+    if (e.target.classList.contains('grid-unit')) {
+        if (erase_toggle.checked) {
+            e.target.style.backgroundColor = "#ffffff"
+        } else if (randomize_toggle.checked) {
+            const randomColor = Math.floor(Math.random() * 16777215).toString(16); 
+            e.target.style.backgroundColor = `#${randomColor.padStart(6, '0')}`;
+        } else {
+            e.target.style.backgroundColor = color_selector.value;
+        }
+    }
+}, true);
+
+clear_btn.addEventListener("click", (e) => {
+    redraw_grid();
+})
+
+canvas_size.value = 32;
 redraw_grid();
